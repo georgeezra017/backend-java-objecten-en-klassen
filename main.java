4bf6960e5a12109a3e264a435099eab708d0384f
georgeezra017 <georgeezra017@gmail.com>
2025-05-28 14:57:07 +0200

    huiswerk-inleverbaar

--- main.java
+++ main.java
@@ -4,8 +4,8 @@ public class main {
         FilmRecipe filmRecipe = new FilmRecipe();
         //Film film = new Film("Pirates", "Johny Depp", "12-03-2011", "action");
 
-        //applePieRecipe.printAllIngredients();
-        //applePieRecipe.printSteps();
+        applePieRecipe.printAllIngredients();
+        applePieRecipe.printSteps();
         filmRecipe.printAllFilms();
         System.out.println();
 

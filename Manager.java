4fa1c5a0e91312885ac885289391a54d39a0486b
georgeezra017 <georgeezra017@gmail.com>
2025-05-30 22:10:35 +0200

    huiswerk af

---
+++ Manager.java
@@ -0,0 +1,23 @@
+public class Manager {
+
+    String name;
+    int age;
+
+    public Manager(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+}
